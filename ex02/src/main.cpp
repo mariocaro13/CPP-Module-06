6d384c65b8e7521193d738ff146261a9c690aa0c
mariocaro13 <mariocaro2002@gmail.com>
2026-03-27 20:07:13 +0100

    Update header comment and adjust pointer declaration style in main.cpp

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mcaro-ro <mcaro-ro@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/27 19:55:11 by mcaro-ro          #+#    #+#             */
-/*   Updated: 2026/03/27 19:55:37 by mcaro-ro         ###   ########.fr       */
+/*   Updated: 2026/03/27 20:06:16 by mcaro-ro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 int main()
 {
-	Base* base = generate();
+	Base *base = generate();
 
 	std::cout << "Identifying with pointer:" << std::endl;
 	identify(base);
